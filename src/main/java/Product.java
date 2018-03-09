b7efea530288d72c4a4014f678c723142e2976c7
Emma Roberts <emrsixtyseven@gmail.com>
2018-03-09 11:19:05 +0000

    initial setup

---
+++ src/main/java/Product.java
@@ -0,0 +1,22 @@
+public class Product {
+
+    private  Double priceBought;
+    private  Double priceSold;
+
+    public Product(Double priceBought, Double priceSold) {
+
+        this.priceBought = priceBought;
+        this.priceSold = priceSold;
+    }
+
+    public Double getPriceBought() {
+        return priceBought;
+    }
+
+    public Double getPriceSold() {
+        return priceSold;
+    }
+    
+
+
+}
