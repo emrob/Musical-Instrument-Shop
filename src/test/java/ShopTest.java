62734f81c56c86ebd56c2ab0c41e02686f39e116
Emma Roberts <emrsixtyseven@gmail.com>
2018-03-10 22:53:59 +0000

    add & remove stock

--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -11,21 +11,15 @@ import static org.junit.Assert.assertNotNull;
 
 public class ShopTest {
      Shop shop;
-//     Piano piano;
+
 
 
     @Before
     public void setUp() throws Exception {
-//        piano = new Piano(350.99, 800.00, InstrumentType.Key, "Yamaha", "Brown");
         shop = new Shop("Emma's Music Shop");
     }
 
-//    @Test
-//    public void checkStock(){
-//        ISell stock = shop.getStock();
-//        assertNotNull(stock);
-//
-//    }
+
 
     @Test
     public void checkStockStartsEmpty(){
@@ -41,7 +35,7 @@ public class ShopTest {
         assertEquals(1, shop.getShopStock().size());
     }
 
-   
+
     @Test
     public void checkCanRemoveStock(){
         Flute flute = new Flute(100.00, 150.00, InstrumentType.Wind, "Muramatsu", "Silver");
